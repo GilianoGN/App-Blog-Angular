c1365e8e438703f7de7500c1764449869c39b30d
Giliano G. Novais <vipergiliano@gmail.com>
2025-05-18 19:04:51 -0300

    Advertising-bar e menu-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdvertisingBarComponent } from './component/advertising-bar/advertising-bar.component'; 
+import { MenuBarComponent } from './component/menu-bar/menu-bar.component';
+import { BigCardComponent } from './component/big-card/big-card.component';
+import { SmallCardsComponent } from './component/small-cards/small-cards.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AdvertisingBarComponent, MenuBarComponent, BigCardComponent, SmallCardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/component/advertising-bar/advertising-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advertising-bar',
+  imports: [],
+  templateUrl: './advertising-bar.component.html',
+  styleUrl: './advertising-bar.component.css'
+})
+export class AdvertisingBarComponent {
+
+}
